03d4a83003c9b1f2a88b407e019132621a82ed00
Toushi8sushi <toshithpu@gmail.com>
2024-06-11 12:10:29 +0530

    Add files via upload

---
+++ Code_with_classes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class semester{
+    public:
+    class course{
+        public:
+        char grade;
+    };
+    int CGPA(){
+        int cgpa;
+        return cgpa;
+    }
+};
+
+void main(){
+    int i=0;
+}
